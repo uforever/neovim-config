3e1c29a177a0dfa118cc7e2308d52b71e71a950a
akashic.zhang <2095138755@qq.com>
2024-05-13 17:47:02 +0800

    v2.0 update

--- lua/options.lua
+++ lua/options.lua
@@ -2,9 +2,14 @@ require "nvchad.options"
 
 local opt = vim.opt
 local wo = vim.wo
+local o = vim.o
 
 wo.relativenumber = true
 opt.whichwrap = "b,s,<,>,[,]"
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldenable = false
+
+if vim.g.neovide then
+  o.guifont = "FiraCode Nerd Font,JetBrainsMono Nerd Font,Cascadia Code:h11"
+end
